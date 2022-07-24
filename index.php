20a499d14738a17b7407e7aa9ad99e380df20f9e
Atmane10 <98719054+Atmane10@users.noreply.github.com>
2022-07-25 01:09:58 +0200

    Update index.php

--- index.php
+++ index.php
@@ -14,23 +14,18 @@
     <div class="form-container">
         <form action="submit.php" method="POST" enctype="multipart/form-data">
             <input type="hidden" name="token" value="HGsZOXpfNC">
+
+            <!-- Les informations génerale -->
             <div class="border border-dark p-3 mb-3">    
-                <h2>Ajouter une photo</h2>
-                <div class="mb-3">
-                    <label class="form-label">Image carrée 1:1 (recommandé)</label>
-                    <input class="form-control" name="profile_image" type="file" required>
-                </div>
-            </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>Contacte</h2>
+                <h2>Informations génerale</h2>
                 <div class="d-flex justify-content-between mb-3">
                     <div>
                         <label class="form-label">Nom</label>
-                        <input type="text" name="first_name" class="form-control" required>
+                        <input type="text" name="nom" class="form-control" required>
                     </div>
                     <div>
                         <label class="form-label">Prénom</label>
-                        <input type="text" name="last_name" class="form-control" required>
+                        <input type="text" name="prenom" class="form-control" required>
                     </div>
                 </div>
                 <div class="mb-3">
@@ -43,15 +38,34 @@
                 </div>
                 <div class="mb-3">
                     <label class="form-label">numéro de téléphone</label>
-                    <input type="tel" class="form-control" id="phone" name="phone" placeholder="+33766223344">
+                    <input type="tel" class="form-control" id="numeroTelephone" name="numeroTelephone" placeholder="0766223344">
                 </div>
             </div>
+
+            <!-- text area pour A propos de moi -->
+            <div class="border border-dark p-3 mb-3">    
+                <h2>A propos de moi</h2>
+                <div class="form-floating">
+                    <textarea class="form-control" name="apropos" style="height: 100px" required></textarea>
+                </div>
+            </div>
+
+            <!-- L'ajoute d'un photo -->
+            <div class="border border-dark p-3 mb-3">    
+                <h2>Ajouter une photo</h2>
+                <div class="mb-3">
+                    <label class="form-label">Image carrée 1:1</label>
+                    <input class="form-control" name="photo" type="file" required>
+                </div>
+            </div>
+
+            <!-- L'ajoute des compétences -->
             <div class="border border-dark p-3 mb-3">    
                 <h2>Compétences</h2>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Nom de la compétence</label>
-                    <input type="text" class="form-control" name="skill1" required>
-                    <select class="form-select mt-2" name="skill_level1" required>
+                    <input type="text" class="form-control" name="competence1" required>
+                    <select class="form-select mt-2" name="competence_niveau1" required>
                         <option value="">Niveau</option>
                         <option value="1">1 - Débutant</option>
                         <option value="2">2 - Moyen</option>
@@ -59,72 +73,74 @@
                         <option value="4">4 - Expert</option>
                     </select>
                 </div>
-                <div id="addSkill"></div>
+                <div id="ajouterCompetences"></div>
                 <div class="mb-3">
-                    <button type="button" id="skill_hide" class="btn btn-primary form-control" onclick="addSkill()">+</button>
+                    <button type="button" id="competences_hide" class="btn btn-primary form-control" onclick="ajouterCompetences()">+</button>
                 </div>
             </div>
+
+            <!-- L'ajoute des loisirs -->
             <div class="border border-dark p-3 mb-3">    
                 <h2>Loisirs</h2>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Loisir</label>
-                    <input type="text" name="hobby1" class="form-control" required>
+                    <input type="text" name="loisir1" class="form-control" required>
                 </div>
-                <div id="addHobby"></div>
+                <div id="ajouterLoisirs"></div>
                 <div class="mb-3">
-                    <button type="button" id="hobby_hide" class="btn btn-primary form-control" onclick="addHobby()">+</button>
+                    <button type="button" id="loisirs_hide" class="btn btn-primary form-control" onclick="ajouterLoisirs()">+</button>
                 </div>
             </div>
-            <div class="border border-dark p-3 mb-3">    
-                <h2>A propos de moi</h2>
-                <div class="form-floating">
-                    <textarea class="form-control" name="about_me" style="height: 100px" required></textarea>
-                </div>
-            </div>
+
+            <!-- L'ajoute de l'éducation -->
             <div class="border border-dark p-3 mb-3">    
                 <h2>Éducation</h2>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Ecole/Université</label>
-                    <input type="text" name="institute1" class="form-control">
+                    <input type="text" name="ecole1" class="form-control">
                 </div>
                 <div class="mb-3">
-                    <label for="exampleInputEmail1" class="form-label">Résultat</label>
-                    <input type="text" name="degree1" class="form-control">
+                    <label for="exampleInputEmail1" class="form-label">Niveau</label>
+                    <input type="text" name="education_niveau1" class="form-control">
                 </div>
                 <div class="mb-3 d-flex justify-content-between">
                     <div>
                         <label for="exampleInputEmail1" class="form-label">Date début</label>
-                        <input type="text" name="from1" class="form-control">
+                        <input type="text" name="dateDebut1" class="form-control">
                     </div>
                     <div>
                         <label for="exampleInputEmail1" class="form-label">Date fin</label>
-                        <input type="text" name="to1" class="form-control">
+                        <input type="text" name="dateFin1" class="form-control">
                     </div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Résultat</label>
-                    <input type="text" name="grade1" class="form-control">
+                    <input type="text" name="resultat1" class="form-control">
                 </div>
-                <div id="addEducation"></div>
+                <div id="ajouterEducation"></div>
                 <div class="mb-3">
-                    <button type="button" id="education_hide" class="btn btn-primary form-control" onclick="addEducation()">+</button>
+                    <button type="button" id="education_hide" class="btn btn-primary form-control" onclick="ajouterEducation()">+</button>
                 </div>
             </div>
+
+            <!-- L'ajoute des expériences -->
             <div class="border border-dark p-3 mb-3">    
                 <h2>Expérience</h2>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Titre</label>
-                    <input type="text" name="title1" class="form-control">
+                    <input type="text" name="titre1" class="form-control">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Description</label>
                     <input type="text" name="description1" class="form-control">
                 </div>
-                <div id="addExperience"></div>
+                <div id="ajouterExperiences"></div>
                 <div class="mb-3">
-                    <button type="button" id="experience_hide" class="btn btn-primary form-control" onclick="addExperience()">+</button>
+                    <button type="button" id="experiences_hide" class="btn btn-primary form-control" onclick="ajouterExperiences()">+</button>
                 </div>
             </div>
+
+            <!-- L'envoie des données et l'affichage du CV -->
             <input type="submit" class="form-control my-2">
         </form>
     </div>
@@ -132,3 +148,4 @@
     <script src="script.js"></script>
 </body>
 </html>
+
